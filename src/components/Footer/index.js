628a7b54d0a61aa23103b04cd75344789d97ad61
vitorgsevero <vitorgsevero@gmail.com>
2020-07-28 14:20:56 -0300

    Updating footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import Logo from '../../assets/img/vitorflix_logo.png';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a
+        href='https://vitorgsevero.github.io/myPortfolio/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <img src={Logo} alt='Logo Vítor' />
       </a>
       <p>
-        Orgulhosamente criado durante a
-        {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
+        Made with{' '}
+        <span role='img' aria-label='heart'>
+          ❤️
+        </span>{' '}
+        by{' '}
+        <a
+          href='https://github.com/vitorgsevero/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Vítor Severo
         </a>
       </p>
     </FooterBase>
