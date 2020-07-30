41b24757eddfc2ce1effbf1013901345252a3ec9
vitorgsevero <vitorgsevero@gmail.com>
2020-07-30 00:15:13 -0300

    Adding Carousel with react-slick library

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './components/Pages/Home';
-import PageNotFound from './components/Pages/PaginaNaoEncontrada/index';
+import PageNotFound from './components/Pages/PaginaNaoEncontrada';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import CadastroVideo from './components/Pages/Cadastro/Video/index';
+import CadastroVideo from './components/Pages/Cadastro/Video';
+import CadastroCategoria from './components/Pages/Cadastro/Categoria';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path='/' component={Home} exact />
+      <Route path='/cadastro/categoria' component={CadastroCategoria} />
       <Route path='/cadastro/video' component={CadastroVideo} />
       <Route component={PageNotFound} />
     </Switch>
