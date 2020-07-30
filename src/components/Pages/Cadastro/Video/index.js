3402f99b032f9a505c6a8bbd0777c4843fd0e7b4
vitorgsevero <vitorgsevero@gmail.com>
2020-07-29 21:51:12 -0300

    First Routes with BrowserRouter, Switch and Route

---
+++ src/components/Pages/Cadastro/Video/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CadastroVideo() {
+  return <div>Cadastro de Vídeos</div>;
+}
+
+export default CadastroVideo;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './components/Pages/Cadastro/Video/index';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path='/' component={App} exact />
+      <Route path='/cadastro/video' component={CadastroVideo} />
+      <Route component={App} />
+    </Switch>
+  </BrowserRouter>,
+
   document.getElementById('root')
 );
